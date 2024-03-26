fce0f39012ba38fe1299b0a723888f1cebf719b4
Siam Islam <siamislam1603@gmail.com>
2024-03-26 15:52:32 +0600

    Update Navigation.js
    1. Nested TabGroup into HomeStack to show the details modal screen in full screen and hid the bottom tab navigator by wrapping the bottom navigator in the Home Stack navigator

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,76 +1,80 @@
-import {Ionicons} from '@expo/vector-icons'
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
-import {NavigationContainer} from '@react-navigation/native'
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
-import React from 'react'
-import {Platform, SafeAreaView, StatusBar, StyleSheet} from 'react-native'
-import TweetDetailScreen from '../screens/homeStack/TweetDetailScreen'
-import Feed from '../screens/tabScreens/Feed'
-import Notifications from '../screens/tabScreens/Notifications'
-import Settings from '../screens/tabScreens/Settings'
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import { Platform, SafeAreaView, StatusBar, StyleSheet } from "react-native";
+import TweetDetailScreen from "../screens/homeStack/TweetDetailScreen";
+import Feed from "../screens/tabScreens/Feed";
+import Notifications from "../screens/tabScreens/Notifications";
+import Settings from "../screens/tabScreens/Settings";
 
 // Stack
-const HomeStack = createNativeStackNavigator()
+const HomeStack = createNativeStackNavigator();
 
 function HomeStackGroup() {
   return (
     <HomeStack.Navigator>
-      <HomeStack.Screen name="Feed" component={Feed} />
+      <HomeStack.Screen
+        name="TabGroup"
+        component={TabGroup}
+        options={{ headerShown: false }}
+      />
       <HomeStack.Screen
         name="TweetDetailScreen"
         component={TweetDetailScreen}
-        options={{presentation: 'modal'}}
+        options={{ presentation: "modal" }}
       />
     </HomeStack.Navigator>
-  )
+  );
 }
 
 // Bottom Tabs
-const Tab = createBottomTabNavigator()
+const Tab = createBottomTabNavigator();
 
 function TabGroup() {
   return (
     <Tab.Navigator
-      screenOptions={({route, navigation}) => ({
-        tabBarIcon: ({size, color, focused}) => {
-          let iconName
-          if (route.name === 'HomeStackGroup') {
-            iconName = focused ? 'home' : 'home-outline'
-          } else if (route.name === 'Settings') {
-            iconName = focused ? 'settings' : 'settings-outline'
-          } else if (route.name === 'Notifications') {
-            iconName = focused ? 'notifications' : 'notifications-outline'
+      screenOptions={({ route, navigation }) => ({
+        tabBarIcon: ({ size, color, focused }) => {
+          let iconName;
+          if (route.name === "Feed") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Settings") {
+            iconName = focused ? "settings" : "settings-outline";
+          } else if (route.name === "Notifications") {
+            iconName = focused ? "notifications" : "notifications-outline";
           }
-          return <Ionicons name={iconName} size={size} color={color} />
+          return <Ionicons name={iconName} size={size} color={color} />;
         },
-        tabBarActiveTintColor: '#1da1f2',
-        tabBarInactiveTintColor: 'gray',
+        tabBarActiveTintColor: "#1da1f2",
+        tabBarInactiveTintColor: "gray",
       })}
     >
       <Tab.Screen
-        name="HomeStackGroup"
-        component={HomeStackGroup}
-        options={{tabBarLabel: 'Home', headerShown: false}}
+        name="Feed"
+        component={Feed}
+        options={{ tabBarLabel: "Home" }}
       />
       <Tab.Screen name="Settings" component={Settings} />
       <Tab.Screen name="Notifications" component={Notifications} />
     </Tab.Navigator>
-  )
+  );
 }
 
 export default function Navigation() {
   return (
     <NavigationContainer>
       <SafeAreaView style={[styles.container]}>
-        <TabGroup />
+        <HomeStackGroup />
       </SafeAreaView>
     </NavigationContainer>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
-})
+});
